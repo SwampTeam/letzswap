2278e7beba116c971772cdce56b636501435c73a
Adolfo <buylla.adolfo@hotmail.com>
2019-04-13 12:32:16 +0200

    base website and homepage merge

--- src/Controller/DefaultController.php
+++ src/Controller/DefaultController.php
@@ -8,11 +8,11 @@ use Symfony\Component\Routing\Annotation\Route;
 class DefaultController extends AbstractController
 {
     /**
-     * @Route("/default", name="default")
+     * @Route("/", name="homepage")
      */
     public function indexAction()
     {
-        return $this->render('default/index.html.twig', [
+        return $this->render('Main/index.html.twig', [
             'controller_name' => 'DefaultController',
         ]);
     }

5851a001e560794e5e4cdcc054369c6f80fd49d4
Marco L Abreu <marcolabreu@users.noreply.github.com>
2019-04-14 12:11:46 +0200

    Update registration form type

--- src/Form/RegistrationFormType.php
+++ src/Form/RegistrationFormType.php
@@ -4,7 +4,12 @@ namespace App\Form;
 
 use App\Entity\User;
 use Symfony\Component\Form\AbstractType;
+use Symfony\Component\Form\Extension\Core\Type\CheckboxType;
+use Symfony\Component\Form\Extension\Core\Type\EmailType;
 use Symfony\Component\Form\Extension\Core\Type\PasswordType;
+use Symfony\Component\Form\Extension\Core\Type\RepeatedType;
+use Symfony\Component\Form\Extension\Core\Type\SubmitType;
+use Symfony\Component\Form\Extension\Core\Type\TextType;
 use Symfony\Component\Form\FormBuilderInterface;
 use Symfony\Component\OptionsResolver\OptionsResolver;
 use Symfony\Component\Validator\Constraints\NotBlank;
@@ -15,30 +20,50 @@ class RegistrationFormType extends AbstractType
     public function buildForm(FormBuilderInterface $builder, array $options)
     {
         $builder
-            ->add('id')
-            ->add('plainPassword', PasswordType::class, [
-                // instead of being set onto the object directly,
-                // this is read and encoded in the controller
-                'mapped' => false,
-                'constraints' => [
-                    new NotBlank([
-                        'message' => 'Please enter a password',
-                    ]),
-                    new Length([
-                        'min' => 6,
-                        'minMessage' => 'Your password should be at least {{ limit }} characters',
-                        // max length allowed by Symfony for security reasons
-                        'max' => 4096,
-                    ]),
-                ],
-            ])
-        ;
+            ->add('username', TextType::class, ['label' => 'FORM.USER.USERNAME'])
+            ->add('password', RepeatedType::class,
+                [
+                    'type' => PasswordType::class,
+                    'mapped' => false,
+                    'constraints' => [
+                        new NotBlank([
+                            'message' => 'FORM.USER.PASSWORD.ERROR.BLANK',
+                        ]),
+                        new Length([
+                            'min' => 8,
+                            'minMessage' => 'FORM.USER.PASSWORD.ERROR.LENGTH',
+                            'max' => 255,
+                        ]),
+                    ],
+                    'invalid_message' => 'FORM.USER.PASSWORD.ERROR.DONT_MATCH',
+                    'first_options' => ['label' => 'FORM.USER.PASSWORD.FIRST'],
+                    'second_options' => ['label' => 'FORM.USER.PASSWORD.REPEAT'],
+                ]
+            )->add(
+                'email',
+                EmailType::class,
+                ['label' => 'FORM.USER.EMAIL']
+            )->add(
+                'tosAccepted',
+                CheckboxType::class,
+                ['label' => 'FORM.USER.TOS_ACCEPTED']
+            );
+        if ($options['standalone']) {
+            $builder->add(
+                'Submit',
+                SubmitType::class,
+                [
+                    'label' => 'FORM.USER.SUBMIT',
+                    'attr' => ['class' => 'btn-success']
+                ]);
+        }
     }
 
     public function configureOptions(OptionsResolver $resolver)
     {
         $resolver->setDefaults([
             'data_class' => User::class,
+            'standalone' => false,
         ]);
     }
 }

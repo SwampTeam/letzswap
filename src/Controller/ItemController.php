a965eda9f9faf050647dc3bf11bd1bbccf544048
Adolfo <buylla.adolfo@hotmail.com>
2019-04-15 22:19:12 +0200

    items handling controller and type

--- src/Controller/ItemController.php
+++ src/Controller/ItemController.php
@@ -2,60 +2,108 @@
 
 namespace App\Controller;
 
-use App\Entity\User;
-use App\Form\NewItemFormType;
 use App\Entity\Item;
-use Ramsey\Uuid\Uuid;
+use App\Form\ItemType;
+use App\Repository\ItemRepository;
 use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
-use Symfony\Component\Form\FormFactoryInterface;
 use Symfony\Component\HttpFoundation\Request;
 use Symfony\Component\HttpFoundation\Response;
 use Symfony\Component\Routing\Annotation\Route;
-use Twig\Environment;
 
+/**
+ * @Route("/item")
+ */
 class ItemController extends AbstractController
 {
     /**
-     * @Route("/newitem", name="new_item")
-     * @param Request $request
-     * @param FormFactoryInterface $formFactory
-     * @param Environment $twig
+     * @Route("/", name="item_index", methods={"GET"})
+     * @param ItemRepository $itemRepository
+     * @return Response
      */
-    public function addItem(Request $request,
-                            FormFactoryInterface $formFactory,
-                            Environment $twig)
+    public function index(ItemRepository $itemRepository): Response
+    {
+        return $this->render('item/index.html.twig', [
+            'items' => $itemRepository->findAll(),
+        ]);
+    }
+
+    /**
+     * @Route("/new", name="item_new", methods={"GET","POST"})
+     * @param Request $request
+     * @return Response
+     */
+    public function new(Request $request): Response
     {
         $item = new Item();
-        $form = $formFactory->create(
-            NewItemFormType::class,
-            $item,
-            ['standalone' => true]
-        );
-
+        $form = $this->createForm(ItemType::class, $item);
         $form->handleRequest($request);
 
         if ($form->isSubmitted() && $form->isValid()) {
-            // open the image, getDate return the uploaded file
-            $file = $form->get('file')->getData();
-            // rename file
-            $fileName = Uuid::uuid4()->toString() . '.' . 'pic';
-            // save the file
-            $item->setTitle();
-            $item->setDescription();
-            $item->setConditionStatus();
-            $item->setUserId($this->getUser());
-            $file->move($this->getParameter('upload_directory'), $fileName);
-
-
-            // get doctrine
+            $item->setUser($this->getUser()->getId());
             $entityManager = $this->getDoctrine()->getManager();
             $entityManager->persist($item);
             $entityManager->flush();
 
-            return $this->redirectToRoute('homepage');
+            return $this->redirectToRoute('item_index');
         }
-        return new Response($twig->render('Item/add-item.html.twig', [
-            'addItemForm' => $form->createView()
-        ]));
+
+        return $this->render('item/new.html.twig', [
+            'item' => $item,
+            'form' => $form->createView(),
+        ]);
+    }
+
+    /**
+     * @Route("/{id}", name="item_show", methods={"GET"})
+     * @param Item $item
+     * @return Response
+     */
+    public function show(Item $item): Response
+    {
+        return $this->render('item/show.html.twig', [
+            'item' => $item,
+        ]);
+    }
+
+    /**
+     * @Route("/{id}/edit", name="item_edit", methods={"GET","POST"})
+     * @param Request $request
+     * @param Item $item
+     * @return Response
+     */
+    public function edit(Request $request, Item $item): Response
+    {
+        $form = $this->createForm(ItemType::class, $item);
+        $form->handleRequest($request);
+
+        if ($form->isSubmitted() && $form->isValid()) {
+            $this->getDoctrine()->getManager()->flush();
+
+            return $this->redirectToRoute('item_index', [
+                'id' => $item->getId(),
+            ]);
+        }
+
+        return $this->render('item/edit.html.twig', [
+            'item' => $item,
+            'form' => $form->createView(),
+        ]);
+    }
+
+    /**
+     * @Route("/{id}", name="item_delete", methods={"DELETE"})
+     * @param Request $request
+     * @param Item $item
+     * @return Response
+     */
+    public function delete(Request $request, Item $item): Response
+    {
+        if ($this->isCsrfTokenValid('delete'.$item->getId(), $request->request->get('_token'))) {
+            $entityManager = $this->getDoctrine()->getManager();
+            $entityManager->remove($item);
+            $entityManager->flush();
+        }
+
+        return $this->redirectToRoute('item_index');
     }
 }
--- src/Form/ItemType.php
+++ src/Form/ItemType.php
@@ -3,47 +3,54 @@
 namespace App\Form;
 
 use App\Entity\Item;
-use App\Entity\Picture;
+use App\Entity\User;
 use Symfony\Bridge\Doctrine\Form\Type\EntityType;
 use Symfony\Component\Form\AbstractType;
+use Symfony\Component\Form\Extension\Core\Type\ChoiceType;
 use Symfony\Component\Form\Extension\Core\Type\FileType;
 use Symfony\Component\Form\Extension\Core\Type\SubmitType;
 use Symfony\Component\Form\Extension\Core\Type\TextareaType;
 use Symfony\Component\Form\Extension\Core\Type\TextType;
 use Symfony\Component\Form\FormBuilderInterface;
 use Symfony\Component\OptionsResolver\OptionsResolver;
+use Symfony\Component\Validator\Constraints\Image;
 
-class NewItemFormType extends AbstractType
+class ItemType extends AbstractType
 {
     public function buildForm(FormBuilderInterface $builder, array $options)
     {
         $builder
-            ->add('username', TextType::class, ['label' => 'NAV.ADD_ITEM.USERNAME.LABEL'])
             ->add('title', TextType::class, ['label' => 'FORM.ITEM.TITLE.LABEL'])
             ->add('description', TextareaType::class,
                 ['label' => 'NAV.ADD_ITEM.DESCRIPTION.LABEL',
                     'required' => false
                 ])
-            ->add('condition_status', EntityType::class, ['label' => 'NAV.ADD_ITEM.STATUS.LABEL',
-                'class' => Item::class,
-                'choice_label' => 'label',
-                'multiple' => true,
-                'required' => false
+            ->add('condition', ChoiceType::class, ['label' => 'NAV.ADD_ITEM.STATUS.LABEL',
+                'choices'  => [
+                    'Brand new' => 'Brand new',
+                    'Used once' => 'Used once',
+                    "Don't work" => "Don't work"
+                ],
+                'multiple' => false,
+                'required' => true,
+                'constraints'=> []
             ])
             ->add('picture', FileType::class,
                 ['label' => 'NAV.ADD_ITEM.PICTURE.LABEL',
                     'mapped' => false,
                     'constraints' => [
-                        new Picture([
+                        new Image([
                             'mimeTypes' => ['image/png', 'image/jpeg'],
-                            'maxSize' => '6M',
+                            'maxSize' => '5M',
                             'minWidth' => 300,
                             'minHeight' => 300
                         ])
                     ]
                 ]
 
-            );
+            )
+//            ->add('getId', EntityType::class, ['label' => 'NAV.ADD_ITEM.USERNAME.LABEL', 'class' => User::class]);
+;
 
         if ($options['standalone']) {
             $builder->add(
@@ -58,7 +65,6 @@ class NewItemFormType extends AbstractType
             );
         }
     }
-
 
     public function configureOptions(OptionsResolver $resolver)
     {

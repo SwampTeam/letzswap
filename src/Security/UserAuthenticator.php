9438d6f31be4bcb11866eb04f526683059054818
fGaman <filipgaman@gmail.com>
2019-04-15 10:05:38 +0200

    Login form

--- src/Security/UserAuthenticator.php
+++ src/Security/UserAuthenticator.php
@@ -90,7 +90,7 @@ class UserAuthenticator extends AbstractFormLoginAuthenticator
         }
 
         // For example : return new RedirectResponse($this->urlGenerator->generate('some_route'));
-        throw new \Exception('TODO: provide a valid redirect inside '.__FILE__);
+        return new RedirectResponse($this->urlGenerator->generate('homepage'));
     }
 
     protected function getLoginUrl()

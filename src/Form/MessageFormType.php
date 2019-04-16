3228bba11d6b703392f1fb32439725efd49e7543
apolline <weirich_apolline@msn.com>
2019-04-16 10:56:54 +0200

    creation of send message form

---
+++ src/Form/MessageFormType.php
@@ -0,0 +1,32 @@
+<?php
+
+namespace App\Form;
+
+use App\Entity\User;
+use Symfony\Component\Form\AbstractType;
+use Symfony\Component\Form\Extension\Core\Type\SubmitType;
+use Symfony\Component\Form\Extension\Core\Type\TextareaType;
+use Symfony\Component\Form\Extension\Core\Type\TextType;
+use Symfony\Component\Form\FormBuilderInterface;
+use Symfony\Component\OptionsResolver\OptionsResolver;
+
+class MessageFormType extends AbstractType
+{
+    public function buildForm(FormBuilderInterface $builder, array $options)
+    {
+        $builder
+            ->add('username', TextType::class, ['label' => 'MESSAGE.USERNAME.LABEL'])
+            ->add('content', TextareaType::class, ['label' => 'MESSAGE.CONTENT.LABEL'])
+            ->add('send', SubmitType::class, ['label' => 'MESSAGE.SEND.LABEL'])
+        ;
+    }
+
+    public function configureOptions(OptionsResolver $resolver)
+    {
+        $resolver->setDefaults([
+            'data_class' => User::class,
+            'standalone' => false
+        ]);
+    }
+
+}

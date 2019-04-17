5487b566d94a80fcf475d6bb8e05cb8c8fbed0ef
Marco L. Abreu <marcolabreu@users.noreply.github.com>
2019-04-17 16:11:02 +0200

    Update contact form

--- src/Form/ContactFormType.php
+++ src/Form/ContactFormType.php
@@ -7,6 +7,7 @@ use Symfony\Component\Form\AbstractType;
 use Symfony\Component\Form\Extension\Core\Type\EmailType;
 use Symfony\Component\Form\Extension\Core\Type\SubmitType;
 use Symfony\Component\Form\Extension\Core\Type\TextareaType;
+use Symfony\Component\Form\Extension\Core\Type\TextType;
 use Symfony\Component\Form\FormBuilderInterface;
 use Symfony\Component\OptionsResolver\OptionsResolver;
 
@@ -16,6 +17,9 @@ class ContactFormType extends AbstractType
     public function buildForm(FormBuilderInterface $builder, array $options)
     {
         $builder
+            ->add('sender',
+                TextType::class,
+                ['label' => 'CONTACT.FORM.EMAIL'])
             ->add('email',
                 EmailType::class,
                 ['label' => 'CONTACT.FORM.EMAIL'])

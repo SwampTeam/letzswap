bda531ef7872a78f92bf508c2f7637567ba2c34c
Adolfo <buylla.adolfo@hotmail.com>
2019-04-18 12:35:08 +0200

    clean test func

--- src/Service/AvatarGenerator.php
+++ src/Service/AvatarGenerator.php
@@ -16,17 +16,5 @@ class AvatarGenerator
 
         return $grav_url;
     }
-
-    public function testFunction()
-    {
-        $messages = [
-            'You did it! You updated the system! Amazing!',
-            'That was one of the coolest updates I\'ve seen all day!',
-            'Great work! Keep going!',
-        ];
-
-        $index = array_rand($messages);
-
-        return $messages[$index];
-    }
+    
 }

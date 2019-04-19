af38765f41363ca7e4af7cbac2cddb3322ae01ee
Adolfo <buylla.adolfo@hotmail.com>
2019-04-19 14:50:09 +0200

    admin add, items ok, users on route

--- src/Controller/AdminController.php
+++ src/Controller/AdminController.php
@@ -1,14 +1,15 @@
 <?php
 
-
 namespace App\Controller;
 
-use App\Entity\User;
+use App\Repository\ItemRepository;
+use App\Repository\PictureRepository;
 use App\Repository\UserRepository;
+use App\Repository\UserStatusRepository;
 use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
 use Symfony\Component\HttpFoundation\Response;
 use Symfony\Component\Routing\Annotation\Route;
-
+use App\Service\AvatarGenerator as AVA;
 
 class AdminController extends AbstractController
 {
@@ -16,17 +17,28 @@ class AdminController extends AbstractController
     /**
      * @Route("/admin", name="admin", methods={"GET"})
      * @param UserRepository $userRepository
+     * @param UserStatusRepository $userStatusRepository
+     * @param ItemRepository $itemRepository
+     * @param PictureRepository $pictureRepository
+     * @param AVA $getGravar
      * @return Response
      */
-    public function getUsers(UserRepository $userRepository): Response
+    public function getUsers(UserRepository $userRepository, UserStatusRepository $userStatusRepository, ItemRepository $itemRepository, PictureRepository $pictureRepository, AVA $getGravar): Response
     {
 
-        if ($this->isGranted('ROLE_ADMIN')) {
-            return $this->render('Admin/admin.html.twig', [
-                'users' => $userRepository->findAll(),
-            ]);
-        }
+// if ($this->isGranted('ROLE_ADMIN')) {
 
-        return $this->redirectToRoute('swamp');
+        //$showGravatar = $getGravar->getAvatar($email, $username, 200);
+        return $this->render('Admin/index.html.twig', [
+            'users' => $userRepository->findAll(),
+            'user_status' => $userStatusRepository->findAll(),
+// 'user_avatar' => $showGravatar,
+            'items' => $itemRepository->findAll(),
+            'pictures' => $pictureRepository->findAll(),
+            'user_role' => $this->getUser()->getRoles()
+        ]);
+// } else {
+//  return $this->redirectToRoute('swamp');
+//  }
     }
 }

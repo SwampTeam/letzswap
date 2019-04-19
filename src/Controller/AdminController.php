a364ea9257096cd11c5421a04d1f583e267812c5
Adolfo <buylla.adolfo@hotmail.com>
2019-04-19 16:30:53 +0200

    add report status and avatar

--- src/Controller/AdminController.php
+++ src/Controller/AdminController.php
@@ -5,7 +5,6 @@ namespace App\Controller;
 use App\Repository\ItemRepository;
 use App\Repository\PictureRepository;
 use App\Repository\UserRepository;
-use App\Repository\UserStatusRepository;
 use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
 use Symfony\Component\HttpFoundation\Response;
 use Symfony\Component\Routing\Annotation\Route;
@@ -17,22 +16,20 @@ class AdminController extends AbstractController
     /**
      * @Route("/admin", name="admin", methods={"GET"})
      * @param UserRepository $userRepository
-     * @param UserStatusRepository $userStatusRepository
      * @param ItemRepository $itemRepository
      * @param PictureRepository $pictureRepository
      * @param AVA $getGravar
      * @return Response
      */
-    public function getUsers(UserRepository $userRepository, UserStatusRepository $userStatusRepository, ItemRepository $itemRepository, PictureRepository $pictureRepository, AVA $getGravar): Response
+    public function getAdmin(UserRepository $userRepository, ItemRepository $itemRepository, PictureRepository $pictureRepository, AVA $getGravar): Response
     {
-
+//        $itemStatus = new ItemStatus();
+//        $itemStatus->setItem($item)->setStatus($status);
+//        $entityManager->persist($itemStatus);
 // if ($this->isGranted('ROLE_ADMIN')) {
-
-        //$showGravatar = $getGravar->getAvatar($email, $username, 200);
         return $this->render('Admin/index.html.twig', [
             'users' => $userRepository->findAll(),
-            'user_status' => $userStatusRepository->findAll(),
-// 'user_avatar' => $showGravatar,
+            'user_avatar' => $getGravar,
             'items' => $itemRepository->findAll(),
             'pictures' => $pictureRepository->findAll(),
             'user_role' => $this->getUser()->getRoles()

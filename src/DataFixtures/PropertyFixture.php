cedc3afc82a14aa44ed8aab1c84a87d5a88512a3
apolline <weirich_apolline@msn.com>
2019-04-20 17:30:34 +0200

    created propertyFixture

---
+++ src/DataFixtures/PropertyFixture.php
@@ -0,0 +1,26 @@
+<?php
+
+namespace App\DataFixtures;
+
+use Doctrine\Bundle\FixturesBundle\Fixture;
+use Doctrine\Common\Persistence\ObjectManager;
+use Faker\Factory;
+use PhpParser\Builder\Property;
+
+class PropertyFixture extends Fixture
+{
+    public function load(ObjectManager $manager)
+    {
+        $faker = Factory::create('en_GB');
+        for ($i = 0; $i < 100; $i++){
+            $property = new Property();
+            $property
+                ->setType($faker->words(2, true))
+                ->setDescription($faker->sentences(3, true))
+                ->setPicture($faker->image(200,200,200));
+            $manager->persist($property);
+        }
+
+        $manager->flush();
+    }
+}

6d0ea7bd45ed6a4063bf763fb1631afecf7522be
Adolfo <buylla.adolfo@hotmail.com>
2019-04-24 13:12:15 +0200

    fix gravatar service

--- src/Service/AvatarGenerator.php
+++ src/Service/AvatarGenerator.php
@@ -2,19 +2,36 @@
 
 namespace App\Service;
 
-class AvatarGenerator
+use App\Entity\User;
+use Doctrine\Bundle\DoctrineBundle\Repository\ServiceEntityRepository;
+use Doctrine\ORM\EntityManagerInterface;
+
+/**
+ * Class AvatarGenerator
+ * @package App\Service
+ */
+class AvatarGenerator extends ServiceEntityRepository
 {
+
     /**
-     * @param string $email
+     * AvatarGenerator constructor.
+     * @param EntityManagerInterface $entityManager
+     */
+    public function __construct(EntityManagerInterface $entityManager)
+    {
+        $this->repository = $entityManager->getRepository(User::class);
+    }
+
+    /**
      * @param string $username
      * @param int $size
      * @return string
      */
-    public function getAvatar(string $email, string $username, int $size): string
+    public function getAvatar(string $username, int $size): string
     {
-        $grav_url = '<img src="https://www.gravatar.com/avatar/' . md5(strtolower(trim($email))) ."?r=". "&s=" . $size . '" alt="' . $username . ' logo">';
-
+        $email = $this->repository->findOneByUsername($username)->getEmail();
+        $grav_url = '<img src="https://www.gravatar.com/avatar/' . md5(strtolower(trim($email))) . "?r=g" . "&s=" . $size . '" alt="' . $username . ' logo">';
         return $grav_url;
     }
-    
+
 }
